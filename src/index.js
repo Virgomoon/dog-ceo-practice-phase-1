de6806a82d5b8af3b6cf8298ef24ef4739cd725e
Joseph Guillory <jagmanx99@yahoo.com>
2022-05-05 23:40:13 -0700

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,67 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded',()=>{
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(obj){
+        
+        obj.message.forEach(element=>{
+        const img = document.createElement('img')
+        imageContainer.appendChild(img) 
+        img.src = element
+        })
+    })
+
+    let imageContainer = document.querySelector(["#dog-image-container"])
+
+    fetch('https://dog.ceo/api/breeds/list/all')
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(obj){
+
+        let objectMessage = obj.message
+        let prefilteredDogs = []
+        let filteredDogs = []
+
+        for(item in objectMessage) {
+            prefilteredDogs.push(item)
+        }
+
+        buildList(prefilteredDogs)
+
+        breedDropdown.addEventListener('change', function(e){
+                while(dogBreeds.firstChild){
+                    dogBreeds.removeChild(dogBreeds.firstChild)
+                }
+                filteredDogs = prefilteredDogs.filter(el => el[0] === e.target.value)
+            buildList(filteredDogs)
+            }
+        )
+            function buildList(list){
+                for(element of list){
+                    const li = document.createElement('li')
+                    dogBreeds.appendChild(li)
+                    li.className = "each-dog"
+                    li.innerHTML = element 
+                    
+                    let eachDog = document.querySelectorAll([".each-dog"])
+                    
+                    eachDog.forEach(dog => dog.addEventListener('click', function(e){
+                        e.target.style.color = 'red'
+                    }))
+                    
+                } 
+            }    
+        })
+        let dogBreeds = document.querySelector(["#dog-breeds"])
+        let breedDropdown = document.querySelector(["#breed-dropdown"])
+    }) 
+        
+        
+ 
+    
+    
+    
+
